f6c811305b9ea868697b8a9f71f2f6e30f244a7d
Htay Lin Aung <htay.dev@gmail.com>
2024-07-29 16:51:24 +0630

    Initial commit

---
+++ README.md
@@ -0,0 +1,141 @@
+
+---
+
+# Google Classroom Course Materials Downloader
+
+This script downloads and organizes course materials from Google Classroom, saving them to your local machine by topic.
+
+## Table of Contents
+
+1. [Prerequisites](#prerequisites)
+2. [Installation](#installation)
+3. [Google Cloud Console Setup](#google-cloud-console-setup)
+4. [Running the Script](#running-the-script)
+5. [Troubleshooting](#troubleshooting)
+
+## Prerequisites
+
+- Python 3.6 or higher
+- Google account with access to Google Classroom and Google Drive
+
+## Installation
+
+### Python Installation
+
+1. **Download Python:**
+   - Go to the [Python download page](https://www.python.org/downloads/).
+   - Download the latest version of Python for your operating system.
+
+2. **Install Python:**
+   - Run the installer and follow the on-screen instructions.
+   - Make sure to check the box that says "Add Python to PATH" during installation.
+
+### Cloning the Repository
+
+1. **Clone the repository:**
+    
+    Clone this repository or download the zip file and extract it. Navigate to the project directory.
+
+   ```sh
+   git clone https://github.com/htaylight/google-classroom-downloader.git
+   cd google-classroom-downloader
+   ```
+
+2. **Install the required Python packages:**
+   ```sh
+   pip install -r requirements.txt
+   ```
+   ![Install Python packages](Screenshots/26.png)
+    
+
+## Google Cloud Console Setup
+
+### 1. Create a Project
+- Go to the [Google Cloud Console](https://console.cloud.google.com/).
+- If it is the very first time at `Google Cloud Console`, choose `Country` and tick at `Terms of Service`> `AGREE AND CONTINUE`  
+  ![Welcome Page](Screenshots/1.png)
+- Click on `Select a project` and click `New Project`.  
+  ![Select a Project](Screenshots/2.png)
+  ![New Project](Screenshots/3.png)
+- Enter a project name, choose `No organization` for location and click `Create`.
+  ![Create Project](Screenshots/4.png)
+### 2. Enable APIs
+- Select your project.
+  ![Select a Project](Screenshots/5.png)
+  ![Select Project](Screenshots/6.png)
+- Go to the API & Services Dashboard.
+  ![API & Services](Screenshots/7.png) 
+  ![Enable APIs](Screenshots/8.png)
+- Search and Enable the following APIs:
+  - Google Classroom API
+  - Google Drive API  
+  ![Enable APIs Step1](Screenshots/9.png)
+  ![Enable APIs Step2](Screenshots/10.png)
+  ![Enable APIs Step3](Screenshots/11.png)
+  ![Enable APIs Step4](Screenshots/12.png)
+  ![Enable APIs Step5](Screenshots/13.png)
+
+### 3. Create OAuth 2.0 Credentials
+- Go to `OAuth consent screen` and select `External` as `User Type` then click `CREATE` button. 
+  And follow the steps as shown in the pictures below.
+  ![OAuth consent screen Step1](Screenshots/14.png)
+  ![OAuth consent screen Step2](Screenshots/15.png)
+  ![OAuth consent screen Step3](Screenshots/16.png)
+  ![OAuth consent screen Step4](Screenshots/17.png)
+  ![OAuth consent screen Step5](Screenshots/18.png)
+  ![OAuth consent screen Step6](Screenshots/19.png)
+  ![OAuth consent screen Step7](Screenshots/20.png)
+  ![OAuth consent screen Step8](Screenshots/21.png)
+- After published, go to `Credentials` and click `CREATE CREDENTIALS`, from drop down list select `OAuth Client ID`. 
+  ![Create Credentials Step1](Screenshots/22.png)
+- Select `Desktop app` as the application type and click `Create`.
+  ![Create Credentials Step2](Screenshots/23.png)
+- Download the `json` file.
+  ![Create Credentials Step3](Screenshots/24.png)
+
+### 4. Save the Credentials File
+- Rename the downloaded json file to `credentials.json` and place the file in the project directory `google-classroom-downloader`.
+
+## Running the Script
+
+1. **Navigate to the project directory:**
+
+   ```sh
+   cd google-classroom-downloader
+   ```
+   
+   (or)Type `cmd` in the address bar of folder and hit ENTER
+   ![cmd on address ](Screenshots/25.png)
+
+2. **Run the script:**
+
+   ```sh
+   python3 download.py
+   ```
+   ![Running](Screenshots/27.png)
+
+3. **Follow the prompts:**
+   - The script will open a browser window for authentication. And follow the steps as shown in pictures below.
+     ![Authentication1](Screenshots/28.png)
+     ![Authentication2](Screenshots/29.png)
+     ![Authentication3](Screenshots/30.png)
+     ![Authentication4](Screenshots/31.png)
+     ![Authentication5](Screenshots/32.png)
+
+
+   - Choose the course you want to download by entering the corresponding number.
+     ![Choose the Course](Screenshots/34.png)
+
+   - The script will download the materials and save them in the `Downloads` folder.
+
+## Troubleshooting
+
+- **Authentication Issues:**
+  - If you encounter issues during authentication, delete the `token.pickle` file and try again.
+  - Make sure your `credentials.json` file is correctly placed in the specified directory.
+
+- **File Download Issues:**
+  - Ensure you have a stable internet connection.
+  - Check for any permission issues on the download directory.
+
+---
---
+++ download.py
@@ -0,0 +1,196 @@
+import os
+import pickle
+import re
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseDownload
+
+# If modifying these SCOPES, delete the file token.pickle.
+SCOPES = [
+    'https://www.googleapis.com/auth/classroom.courses.readonly',
+    'https://www.googleapis.com/auth/classroom.courseworkmaterials.readonly',
+    'https://www.googleapis.com/auth/classroom.topics.readonly',
+    'https://www.googleapis.com/auth/drive.readonly'
+]
+
+
+def get_credentials():
+    creds = None
+    token_path = 'token.pickle'
+    creds_path = 'credentials.json'
+
+    if os.path.exists(token_path):
+        with open(token_path, 'rb') as token:
+            creds = pickle.load(token)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(creds_path, SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open(token_path, 'wb') as token:
+            pickle.dump(creds, token)
+    return creds
+
+
+def list_courses(classroom_service):
+    results = classroom_service.courses().list(pageSize=10).execute()
+    courses = results.get('courses', [])
+    return courses
+
+
+def get_course_materials(service, course_id):
+    try:
+        materials_response = service.courses().courseWorkMaterials().list(courseId=course_id).execute()
+        materials = materials_response.get('courseWorkMaterial', [])
+        return materials
+    except Exception as e:
+        print(f'An error occurred while fetching materials: {e}')
+        return []
+
+
+def extract_file_id_from_url(url):
+    match = re.search(r'/d/([a-zA-Z0-9_-]+)', url)
+    return match.group(1) if match else None
+
+
+def download_file(drive_service, file_id, file_path, failed_downloads):
+    if os.path.exists(file_path):
+        print(f"File {file_path} already exists. Skipping download.\n")
+        return True
+    try:
+        request = drive_service.files().get_media(fileId=file_id)
+        with open(file_path, 'wb') as file:
+            downloader = MediaIoBaseDownload(file, request)
+            done = False
+            while not done:
+                status, done = downloader.next_chunk()
+                if status:
+                    print(f"Downloaded {int(status.progress() * 100)}%.")
+        print(f'File downloaded to {file_path}\n')
+        return True
+    except Exception as e:
+        print(f'An error occurred while downloading the file: {e}\n')
+        failed_downloads.append((file_path, str(e)))
+        return False
+
+
+def main():
+    creds = get_credentials()
+    classroom_service = build('classroom', 'v1', credentials=creds)
+    drive_service = build('drive', 'v3', credentials=creds)
+
+    download_path = os.path.join(os.path.expanduser('~'), 'Downloads')
+
+    courses = list_courses(classroom_service)
+
+    if not courses:
+        print('No courses found.')
+        return
+
+    print("Choose the course to download:")
+    for idx, course in enumerate(courses, start=1):
+        print(f"{idx}: {course['name']}")
+
+    while True:
+        try:
+            choice = int(input("Enter the number of the course to download: "))
+            if 1 <= choice <= len(courses):
+                break
+            else:
+                print("Invalid choice. Please enter a number corresponding to the course.")
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
+    chosen_course = courses[choice - 1]
+    course_name = chosen_course['name']
+    course_id = chosen_course['id']
+    print(f"Course ID found: {course_id}")
+
+    try:
+        course_folder = os.path.join(download_path, course_name)
+        os.makedirs(course_folder, exist_ok=True)
+
+        topics_response = classroom_service.courses().topics().list(courseId=course_id).execute()
+        topics = topics_response.get('topic', [])
+        topic_dict = {topic['topicId']: topic['name'] for topic in topics}
+
+        if not topics:
+            print('No topics found.')
+        else:
+            print('Downloading materials:')
+            materials = get_course_materials(classroom_service, course_id)
+            if not materials:
+                print('No materials found.')
+            else:
+                materials.reverse()
+
+                total_downloaded_topics = []
+                total_downloaded_files = 0
+                failed_downloads = []
+
+                for material in materials:
+                    topic_id = material.get('topicId')
+                    topic_name = topic_dict.get(topic_id, 'No Topic')
+                    topic_folder = os.path.join(course_folder, topic_name)
+                    os.makedirs(topic_folder, exist_ok=True)
+
+                    print(f"Downloading materials for topic: {topic_name}")
+                    if topic_id not in total_downloaded_topics:
+                        total_downloaded_topics.append(topic_id)
+
+                    if 'materials' in material:
+                        for mat in material['materials']:
+                            if 'driveFile' in mat:
+                                drive_file = mat['driveFile']['driveFile']
+                                file_name = drive_file.get('title')
+                                file_id = drive_file.get('id')
+                                print(f"Found file: {file_name}")
+                                if file_name and file_id:
+                                    file_path = os.path.join(topic_folder, file_name)
+                                    if download_file(drive_service, file_id, file_path, failed_downloads):
+                                        total_downloaded_files += 1
+                                else:
+                                    print(f"Missing file name or ID for material: {mat}")
+                                    failed_downloads.append((file_name, 'Missing file name or ID'))
+                            else:
+                                print(f"No drive file found in material: {mat}")
+                                failed_downloads.append((str(mat), 'No drive file found'))
+                    elif 'description' in material:
+                        description = material.get('description')
+                        file_id_match = re.search(r'/d/([a-zA-Z0-9-_]+)', description)
+                        if file_id_match:
+                            file_id = file_id_match.group(1)
+                            drive_file = drive_service.files().get(fileId=file_id).execute()
+                            file_name = drive_file.get('name')
+                            print(f"Found file: {file_name}")
+                            if file_name and file_id:
+                                file_path = os.path.join(topic_folder, file_name)
+                                if download_file(drive_service, file_id, file_path, failed_downloads):
+                                    total_downloaded_files += 1
+                            else:
+                                print(f"Could not extract file ID from description: {description}")
+                                failed_downloads.append((description, 'Could not extract file ID'))
+                        else:
+                            print(f"Could not extract file ID from description: {description}")
+                            failed_downloads.append((description, 'Could not extract file ID'))
+                    else:
+                        print(f"No materials or description found in material: {material}")
+                        failed_downloads.append((str(material), 'No materials or description found'))
+
+                print("\nDownload Summary:")
+                print(f"Total downloaded topics: {len(total_downloaded_topics)}")
+                print(f"Total downloaded files: {total_downloaded_files}")
+                print(f"Total failed downloads: {len(failed_downloads)}")
+                if failed_downloads:
+                    print("\nFailed Downloads Details:")
+                    for fail in failed_downloads:
+                        print(f"File: {fail[0]}, Reason: {fail[1]}")
+
+    except Exception as e:
+        print(f'An error occurred: {e}')
+
+
+if __name__ == '__main__':
+    main()
